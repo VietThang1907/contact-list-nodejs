f50b57945c9ae85f9ff5c16948b482458ad79b5b
VietThang1907 <hovietthang1907@gmail.com>
2025-05-28 10:14:22 +0700

    2280602990_HoVietThang: Sửa Form thêm liên hệ, bổ sung trường Email

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ app.get('/add-contact', (req, res) => {
   res.sendFile(__dirname + '/views/addContact.html');
 });
 
-// Xử lý form thêm liên hệ
 app.post('/add-contact', (req, res) => {
-  const { name, phone } = req.body;
-  // TODO: Lưu thông tin liên hệ vào database hoặc file
-  console.log('Thêm liên hệ:', name, phone);
-  res.send(`<p>Đã thêm liên hệ: ${name} - ${phone}</p><a href="/">Quay lại</a>`);
+  const { name, phone, email } = req.body;
+  // TODO: Lưu thông tin liên hệ, bao gồm cả email
+  console.log('Thêm liên hệ:', name, phone, email);
+  res.send(`<p>Đã thêm liên hệ: ${name} - ${phone} - ${email}</p><a href="/">Quay lại</a>`);
 });
+
 
 app.listen(3000, () => {
   console.log('Server đang chạy tại http://localhost:3000');
